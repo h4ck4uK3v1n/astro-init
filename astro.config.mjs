a45f43d9935da339c2b2b8052f3281d3e438b5f1
k3v1n <krodriguezcondori34@gmai.com>
2024-12-10 03:55:16 -0400

    astro testimonials

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import alpine from "@astrojs/alpinejs";
 
-import tailwind from '@astrojs/tailwind';
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-custom.netlify.app/",
-  integrations: [tailwind()]
+  integrations: [tailwind(), alpine({ entrypoint: "/src/scripts/entrypoint" }), solidJs()],
+  env: {
+    schema: {
+      API_STRAPI_URL: envField.string({
+        context: "server",
+        access: "public",
+        optional: false,
+      }),
+      IMAGE_STRAPI_URL: envField.string({
+        context: "server",
+        access: "public",
+        optional: false,
+      }),
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        marquee: "marquee var(--duration, 30s) linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          to: { transform: "translateX(-50%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
